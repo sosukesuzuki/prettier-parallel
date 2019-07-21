c5b1bdc57abdfc0ade29924f9c0ed554ea34a35f
Sosuke Suzuki <aosukeke@gmail.com>
2019-07-22 02:36:12 +0900

    Modify to remove ts-node

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("foo");
+import { Worker, isMainThread, parentPort } from "worker_threads";
+
+if (isMainThread) {
+  const worker = new Worker(__filename);
+  worker.on("message", (msg: string) => {
+    console.log(msg);
+  });
+} else {
+  if (parentPort) {
+    parentPort.postMessage("Hello world!");
+  }
+}
