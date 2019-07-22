43a3aec688522f4ace06187e01717f3efabdb934
Sosuke Suzuki <aosukeke@gmail.com>
2019-07-23 04:13:16 +0900

    Remove unused args

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@ import program from "commander";
 import createWorker from "./createWorker";
 
 export function run() {
-  program
-    .version("1.0.0")
-    .arguments("<filename>")
-    .parse(process.argv);
+  program.version("1.0.0").parse(process.argv);
 
   for (let i = 0; i < program.args.length; i++) {
     createWorker(program.args[i]);
