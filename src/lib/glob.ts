2381382846163b24619653f24dc3540dabeaf77d
Sosuke Suzuki <aosukeke@gmail.com>
2019-09-28 23:52:39 +0900

    Modify to enable ignore with gitignore

--- src/lib/glob.ts
+++ src/lib/glob.ts
@@ -2,7 +2,7 @@ import globby from 'globby';
 
 export async function glob(pattern: string): Promise<string[]> {
     // TODO: ignore files by prettierignore
-    const files = await globby(pattern);
+    const files = await globby(pattern, { gitignore: true });
 
     return files;
 }
