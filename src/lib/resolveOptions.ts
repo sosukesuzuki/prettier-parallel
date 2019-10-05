26bbc241545b1d2b960d1c78419201b3d7a7da39
Sosuke Suzuki <aosukeke@gmail.com>
2019-10-05 22:24:09 +0900

    Update types for Prettier

--- src/lib/resolveOptions.ts
+++ src/lib/resolveOptions.ts
@@ -1,8 +1,11 @@
-import prettier from 'prettier';
+import { resolveConfig, resolveConfigFile, Options } from 'prettier';
 
-export async function resolveOptions(): Promise<prettier.Options> {
-    const configFile = await (prettier as any).resolveConfigFile();
-    const options = await prettier.resolveConfig(configFile);
+export async function resolveOptions(): Promise<Options> {
+    const configFile = await resolveConfigFile();
+    if (!configFile) {
+        return {};
+    }
+    const options = await resolveConfig(configFile);
 
     return options || {};
 }
